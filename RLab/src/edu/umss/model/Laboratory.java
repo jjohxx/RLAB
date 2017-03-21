874866aa688b7a10405b729c16310e29b41cbc8b
KevinT <KevinT@kevint.localdomain>
2017-03-20 23:40:05 -0400

    Fixed fields names

--- RLab/src/edu/umss/model/Laboratory.java
+++ RLab/src/edu/umss/model/Laboratory.java
@@ -1,10 +1,24 @@
 package edu.umss.model;
 
 public class Laboratory {
-	
-	private int LabId;
-	private String LabName;
-	
-	
-	
+
+	private int labId;
+	private String labName;
+
+	public Laboratory(int newId) {
+		labId = newId;
+	}
+
+	public String getLabName() {
+		return labName;
+	}
+
+	public void setLabName(String labName) {
+		this.labName = labName;
+	}
+
+	public int getLabId() {
+		return labId;
+	}
+
 }
